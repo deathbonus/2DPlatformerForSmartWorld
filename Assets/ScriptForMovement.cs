331b1554579cbb5e5271dbf40ba59f7da530bb08
deathbonus <deathbonus@yandex.ru>
2020-03-26 14:15:04 +0700

    Add files via upload

---
+++ Assets/ScriptForMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptForMovement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(5, 5);
+    private Vector2 movement;
+    Rigidbody2D star;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+        star = GetComponent<Rigidbody2D>();
+        star.velocity = movement;
+        star.gravityScale = 1;
+    }
+}
